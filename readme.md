a840d97b1eb0d78fdc041a1547a6dcfd4daf403c
Tr.andreson <andresolut@gmail.com>
2023-10-24 10:07:10 +0530

    http-server new routes

---
+++ readme.md
@@ -0,0 +1,17 @@
+<!-- before that all the basic  will be read only will do later-->
+<!-- page 14 start -->
+
+project setup
+creating a new project
+put it in github 
+
+
+<!-- important  -->
+<!-- innter development loop -->
+Make a change
+Compile the application
+Run Tests
+Run the application
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use  actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use  actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 
+// healeth check handler
+async fn healt_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("world");
     format!("Hello {}!",&name)
 }
 
+
 #[tokio::main]
 async fn  main() -> std::io::Result<()> {
+    println!("Server running at port 8000");
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(greet)).route("/{name}", web::get().to(greet))
+        App::new().route("/", web::get().to(greet)).route("/{name}", web::get().to(greet)).route("/health_check", web::get().to(healt_check))
     }).bind("127.0.0.1:8000")?.run().await
     
 }
